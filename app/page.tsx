a0f477f0049e297df179016691405c9e159fc78b
MaviUK <141088900+MaviUK@users.noreply.github.com>
2026-05-23 20:15:26 +0100

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div className="p-10">
-      <div className="rounded-2xl border p-6 text-xl font-bold">
-        Tailwind is working ✅
-      </div>
-    </div>
-  );
-}
